0c411eb76725c7cb5cc5f3f99dd4d3c9e3e1151d
seongsukwon-moreh <seongsu.kwon@moreh.io>
2025-10-31 18:00:31 +0900

    Fix not to use pool_pre_ping on StaticPool, restore pool_recycle

--- sky/utils/db/db_utils.py
+++ sky/utils/db/db_utils.py
@@ -387,13 +387,13 @@ def get_engine(db_name: str):
                         sqlalchemy.create_engine(
                             conn_string,
                             poolclass=sqlalchemy.pool.NullPool,
-                            pool_pre_ping=True))
+                            pool_pre_ping=True,
+                            pool_recycle=1800))
                 elif _max_connections == 1:
                     _postgres_engine_cache[conn_string] = (
                         sqlalchemy.create_engine(
                             conn_string,
-                            poolclass=sqlalchemy.pool.StaticPool,
-                            pool_pre_ping=True))
+                            poolclass=sqlalchemy.pool.StaticPool))
                 else:
                     _postgres_engine_cache[conn_string] = (
                         sqlalchemy.create_engine(
@@ -401,13 +401,14 @@ def get_engine(db_name: str):
                             poolclass=sqlalchemy.pool.QueuePool,
                             size=_max_connections,
                             max_overflow=0,
-                            pool_pre_ping=True))
+                            pool_pre_ping=True,
+                            pool_recycle=1800))
             engine = _postgres_engine_cache[conn_string]
     else:
         db_path = os.path.expanduser(f'~/.sky/{db_name}.db')
         pathlib.Path(db_path).parents[0].mkdir(parents=True, exist_ok=True)
         if db_path not in _sqlite_engine_cache:
             _sqlite_engine_cache[db_path] = sqlalchemy.create_engine(
-                'sqlite:///' + db_path, pool_pre_ping=True)
+                'sqlite:///' + db_path, pool_pre_ping=True, pool_recycle=1800)
         engine = _sqlite_engine_cache[db_path]
     return engine
