c04c793c70245425e96d8e2e35ceb64457e5ce22
seongsukwon-moreh <seongsu.kwon@moreh.io>
2025-12-19 18:38:18 +0900

    Remove tt logic which is handled on get_node_accelerator_count

--- sky/catalog/kubernetes_catalog.py
+++ sky/catalog/kubernetes_catalog.py
@@ -226,21 +226,9 @@ def _list_accelerators(
                     continue
 
                 # Generate the accelerator quantities
-                # For Tenstorrent NPU, read the resource count directly from
-                # tenstorrent.com/npu resource instead of using get_node_accelerator_count
-                # which may prioritize GPU resources
-                if kubernetes_utils.is_tenstorrent_npu(accelerator_name):
-                    if (kubernetes_utils.TENSTORRENT_NPU_RESOURCE_KEY in
-                        node.status.allocatable):
-                        accelerator_count = int(
-                            node.status.allocatable[
-                                kubernetes_utils.TENSTORRENT_NPU_RESOURCE_KEY])
-                    else:
-                        accelerator_count = 0
-                else:
-                    accelerator_count = (
-                        kubernetes_utils.get_node_accelerator_count(
-                            context, node.status.allocatable))
+                accelerator_count = (
+                    kubernetes_utils.get_node_accelerator_count(
+                        context, node.status.allocatable))
 
                 if accelerator_count > 0:
                     # TPUs are counted in a different way compared to GPUs.
@@ -250,7 +238,6 @@ def _list_accelerators(
                         accelerators_qtys.add(
                             (accelerator_name, accelerator_count))
                     else:
-                        # For Tenstorrent NPU and GPUs, generate quantity list
                         count = 1
                         while count <= accelerator_count:
                             accelerators_qtys.add((accelerator_name, count))
